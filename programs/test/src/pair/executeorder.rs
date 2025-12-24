e907438e406a2e8183de5e6ba4ba382a6f36b5b5
Nok1luck1 <kolambo4ka@gmail.com>
2025-12-24 23:11:25 +0000

    кеикеи

--- programs/test/src/pair/executeorder.rs
+++ programs/test/src/pair/executeorder.rs
@@ -1,3 +1,14 @@
 use crate::order;
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 use order::Order;
+
+#[derive(Accounts)]
+#[instruction(id:u64)]
+pub struct ExecuteOrder<'info> {
+    #[account(mut)]
+    pub receiver: Signer<'info>,
+}
--- programs/test/src/pair/order.rs
+++ programs/test/src/pair/order.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    token::{transfer_checked, TransferChecked},
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
 #[account]
 #[derive(InitSpace)]
@@ -27,4 +31,23 @@ pub enum StatusOrder {
 pub struct OrderId {
     pub counter: u64,
     pub bump: u8,
+}
+
+pub fn transfer_tokens<'info>(
+    from: &InterfaceAccount<'info, TokenAccount>,
+    to: &InterfaceAccount<'info, TokenAccount>,
+    amount: &u64,
+    mint: &InterfaceAccount<'info, Mint>,
+    authority: &Signer<'info>,
+    token_program: &Interface<'info, TokenInterface>,
+) -> Result<()> {
+    let transfer_accounts_options = TransferChecked {
+        from: from.to_account_info(),
+        mint: mint.to_account_info(),
+        to: to.to_account_info(),
+        authority: authority.to_account_info(),
+    };
+
+    let cpi_context = CpiContext::new(token_program.to_account_info(), transfer_accounts_options);
+    transfer_checked(cpi_context, *amount, mint.decimals)
 }
