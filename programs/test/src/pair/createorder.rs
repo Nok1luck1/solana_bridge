d11f52d27154c6df8297c82fb1d65f7852eb994a
Nok1luck1 <kolambo4ka@gmail.com>
2026-01-05 15:17:05 +0000

    ergerg

--- programs/test/src/pair/createorder.rs
+++ programs/test/src/pair/createorder.rs
@@ -1,4 +1,4 @@
-use crate::{test::Vault, Order};
+use crate::{test::Vault, Order, OrderId};
 use anchor_lang::prelude::*;
 use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
@@ -8,37 +8,44 @@ pub struct CreateOrder<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     #[account(
-        seeds = [b"vault",vault.owner.key().as_ref()],
-        bump = vault.bump)]
-    pub vault: Account<'info, Vault>,
+        init_if_needed,
+        payer = user,
+        space = 8 + OrderId::INIT_SPACE,
+        seeds = [b"order_id"],
+        bump
+    )]
+    pub order_id: Account<'info, OrderId>,
     #[account(
         init,
-        space = 8 + Order::INIT_SPACE,
         payer = user,
-        seeds = [b"order",user.key().as_ref(),id.to_le_bytes().as_ref()],
+        space = 8 + Order::INIT_SPACE,
+        seeds = [b"order",user.key().as_ref(),order_id.counter.to_le_bytes().as_ref()],
         bump
     )]
     pub order: Account<'info, Order>,
     #[account(
         mint::token_program = token_program
     )]
-    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_0_mint: InterfaceAccount<'info, Mint>,
     #[account(
         mut,
-        token::mint = mint,
-        token::authority = vault,
-        token::token_program = token_program
-    )]
-    pub vault_token_account: InterfaceAccount<'info, TokenAccount>,
-    #[account(
-        mut,
-        token::mint = mint,
+        token::mint = token_0_mint,
         token::authority = user,
         token::token_program = token_program
     )]
-    pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub maker_token_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        token::mint = token_0_mint,
+        token::authority = order,
+        token::token_program = token_program,
+        seeds = [b"vault",token_0_mint.key().as_ref()],
+        bump
+    )]
+    pub vault_token_account: InterfaceAccount<'info, TokenAccount>,
     pub token_program: Interface<'info, TokenInterface>,
-    pub system_account: Program<'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
 // // pub fn create_order(ctx: Context<CreateOrder>, id: u64) -> Result<()> {
