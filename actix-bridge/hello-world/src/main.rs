79e565734c49f14d9f2c6da848449d1e1ee5cb85
Nok1luck1 <kolambo4ka@gmail.com>
2026-01-08 17:52:19 +0200

    fixed bnb provider init

--- actix-bridge/hello-world/src/main.rs
+++ actix-bridge/hello-world/src/main.rs
@@ -15,10 +15,9 @@ async fn main() -> std::io::Result<()> {
 
     let provider_data = web::Data::new(provider);
     HttpServer::new(move || {
-        App::new()
-            .app_data(provider_data.clone()) // Клонируется только Arc
-            .route("/block", web::get().to(get_block))
-            .route("/balance/{address}", web::get().to(get_balance))
+        App::new().app_data(provider_data.clone()) // Клонируется только Arc
+        // .route("/block", web::get().to(get_block))
+        // .route("/balance/{address}", web::get().to(get_balance))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
