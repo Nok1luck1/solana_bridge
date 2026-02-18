6ad8978268867b7d97a9ae3b97280ec01f42d85c
Nok1luck1 <kolambo4ka@gmail.com>
2026-02-18 18:07:38 +0000

    киуикуки

--- app/bridge_app_sol/src/main.rs
+++ app/bridge_app_sol/src/main.rs
@@ -19,14 +19,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let payer = read_keypair_file("../../bridge/tests/keys/admin1.json")?;
     let client = Client::new(Cluster::Localnet, Rc::new(payer));
     let program: anchor_client::Program<Rc<Keypair>> = client.program(bridge::ID)?;
-    let (admin_config_pda, bump) = Pubkey::find_program_address(&[b"admin_config"], &bridge::ID);
+    let (admin_config_pda, _bump) = Pubkey::find_program_address(&[b"admin_config"], &bridge::ID);
     println!("{},pda admin config", admin_config_pda);
     let admin_config: bridge::AdminConfig = program.account(admin_config_pda).await?;
     //return empty because not inited yet
     println!("{:?},admin config admins", admin_config.admins);
     println!("{:?},admin config settet", admin_config.settet);
     println!("{:?},admin config bump", admin_config.bump);
-    let check = utils::get_order_id(&program).await?;
-    println!("{:?},{:?}", check.bump, check.counter);
+    // let check = utils::get_order_id(&program).await?;
+    // println!("{:?},{:?}", check.bump, check.counter);
     Ok(())
 }
--- app/bridge_app_sol/src/utils.rs
+++ app/bridge_app_sol/src/utils.rs
@@ -9,21 +9,53 @@ use anchor_client::{
     Client, Cluster,
 };
 use anchor_lang::prelude::Pubkey;
-use bridge::{instruction, order, AdminConfig, OrderId};
+use anchor_spl::associated_token;
+use anyhow::Ok;
+use bridge::{instruction, order, AdminConfig, Order, OrderId};
 
-pub async fn get_order_id(
+pub async fn get_current_order_id(
     program: &anchor_client::Program<Rc<Keypair>>,
-) -> Result<(OrderId), Box<dyn std::error::Error>> {
-    let (order_id_pda, bump) = Pubkey::find_program_address(&[b"order_id"], &bridge::ID);
+) -> Result<OrderId, anyhow::Error> {
+    let (order_id_pda, _) = Pubkey::find_program_address(&[b"order_id"], &bridge::ID);
     println!("{:?}", order_id_pda);
     let order_id_account: bridge::OrderId = program.account(order_id_pda).await?;
     Ok(order_id_account)
 }
 pub async fn get_admin_config(
     program: &anchor_client::Program<Rc<Keypair>>,
-) -> Result<(AdminConfig), Box<dyn std::error::Error>> {
-    let (admin_config_pda, bump) = Pubkey::find_program_address(&[b"admin_config"], &bridge::ID);
+) -> Result<AdminConfig, anyhow::Error> {
+    let (admin_config_pda, _) = Pubkey::find_program_address(&[b"adminconfig"], &bridge::ID);
     println!("{:?}", admin_config_pda);
     let admin_config_account: bridge::AdminConfig = program.account(admin_config_pda).await?;
     Ok(admin_config_account)
+}
+pub async fn get_specific_order(
+    program: &anchor_client::Program<Rc<Keypair>>,
+    user: Pubkey,
+    order_counter: u64,
+) -> Result<Order, anyhow::Error> {
+    let (order_pda, _) = Pubkey::find_program_address(
+        &[b"order", user.as_ref(), &order_counter.to_le_bytes()],
+        &bridge::ID,
+    );
+    println!("{:?}", order_pda);
+    let order_account: bridge::Order = program.account(order_pda).await?;
+    println!(
+        "{:?},{:?},{:?},",
+        order_account.id, order_account.maker, order_account.timestart
+    );
+    Ok(order_account)
+}
+pub async fn get_token_vault(
+    program: &anchor_client::Program<Rc<Keypair>>,
+    token_mint: Pubkey,
+    token_program_id: Pubkey,
+) -> Result<Pubkey, anyhow::Error> {
+    let (admin_config_pda, _) = Pubkey::find_program_address(&[b"adminconfig"], &bridge::ID);
+    let vault_account = associated_token::get_associated_token_address_with_program_id(
+        &admin_config_pda,
+        &token_mint,
+        &token_program_id,
+    );
+    Ok(vault_account)
 }
