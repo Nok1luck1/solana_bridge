226c2b99ffbbcd3d447bd74930dca2ffde4b2c9c
Nok1luck1 <kolambo4ka@gmail.com>
2026-02-19 01:58:43 +0000

    execution temp wrote

--- app/bridge_app_sol/src/sender.rs
+++ app/bridge_app_sol/src/sender.rs
@@ -1,3 +1,66 @@
-async fn execute_order() -> Result<(), Box<dyn std::error::Error>> {
+use crate::utils;
+use anchor_client::{
+    solana_sdk::{
+        signature::{read_keypair_file, Keypair},
+        signer::Signer,
+        system_program,
+    },
+    Client, Cluster,
+};
+use anchor_lang::{accounts, prelude::Pubkey};
+use anchor_spl::associated_token;
+use anyhow::Ok;
+use bridge::{instruction, order};
+use std::time::SystemTime;
+use std::{rc::Rc, time::UNIX_EPOCH};
+
+async fn execute_order(
+    admin: Keypair,
+    program: &anchor_client::Program<Rc<Keypair>>,
+    token_mint: Pubkey,
+    associated_token_account: Pubkey,
+    amount0: u64,
+    amount1: u64,
+    token0: String,
+    maker: String,
+    receiver: Pubkey,
+) -> Result<(), anyhow::Error> {
+    let timeend = SystemTime::now().elapsed().unwrap().as_secs() as i64;
+    let (order_id_pda, order_id) = utils::get_current_order_id(&program).await?;
+    let (admin_config_pda, admin_config) = utils::get_admin_config(&program).await?;
+    let (order_execution, bump_exec) = Pubkey::find_program_address(
+        &[
+            b"order_execution",
+            admin.pubkey().as_array().as_ref(),
+            &order_id.counter.to_le_bytes(),
+        ],
+        &bridge::ID,
+    );
+    let user_ata = utils::get_user_ata(token_mint, receiver).await?;
+    let vault_ata = utils::get_token_vault(program, token_mint, associated_token_account).await?;
+    let send_transaction = program
+        .request()
+        .accounts(bridge::accounts::ExecuteOrder {
+            order_id: order_id_pda, // PDA order_id
+            order_execution,        // PDA order_execution
+            token_1_mint: token_mint,
+            receiver_token_account: user_ata,
+            vault_token_program: vault_ata,
+            admin: admin.pubkey(),
+            admin_config: admin_config_pda,
+            token_program: token_mint,
+            system_program: bridge::ID,
+            associated_token_program: anchor_spl::associated_token::ID,
+        })
+        .args(bridge::instruction::OrderForExecution {
+            receiver: receiver,
+            token0amount: amount0,
+            token1amount: amount1,
+            token0: token0,
+            sender: maker,
+            timestart: timeend,
+        })
+        .signer(admin)
+        .send();
     Ok(())
 }
--- app/bridge_app_sol/src/utils.rs
+++ app/bridge_app_sol/src/utils.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 use anchor_client::{
     solana_sdk::{
         signature::{read_keypair_file, Keypair},
@@ -9,31 +7,32 @@ use anchor_client::{
     Client, Cluster,
 };
 use anchor_lang::prelude::Pubkey;
-use anchor_spl::associated_token;
+use anchor_spl::associated_token::{self, get_associated_token_address};
 use anyhow::Ok;
 use bridge::{instruction, order, AdminConfig, Order, OrderId};
+use std::rc::Rc;
 
 pub async fn get_current_order_id(
     program: &anchor_client::Program<Rc<Keypair>>,
-) -> Result<OrderId, anyhow::Error> {
+) -> Result<(Pubkey, OrderId), anyhow::Error> {
     let (order_id_pda, _) = Pubkey::find_program_address(&[b"order_id"], &bridge::ID);
     println!("{:?}", order_id_pda);
     let order_id_account: bridge::OrderId = program.account(order_id_pda).await?;
-    Ok(order_id_account)
+    Ok((order_id_pda, order_id_account))
 }
 pub async fn get_admin_config(
     program: &anchor_client::Program<Rc<Keypair>>,
-) -> Result<AdminConfig, anyhow::Error> {
+) -> Result<(Pubkey, AdminConfig), anyhow::Error> {
     let (admin_config_pda, _) = Pubkey::find_program_address(&[b"adminconfig"], &bridge::ID);
     println!("{:?}", admin_config_pda);
     let admin_config_account: bridge::AdminConfig = program.account(admin_config_pda).await?;
-    Ok(admin_config_account)
+    Ok((admin_config_pda, admin_config_account))
 }
 pub async fn get_specific_order(
     program: &anchor_client::Program<Rc<Keypair>>,
     user: Pubkey,
     order_counter: u64,
-) -> Result<Order, anyhow::Error> {
+) -> Result<(Pubkey, bridge::Order), anyhow::Error> {
     let (order_pda, _) = Pubkey::find_program_address(
         &[b"order", user.as_ref(), &order_counter.to_le_bytes()],
         &bridge::ID,
@@ -44,18 +43,22 @@ pub async fn get_specific_order(
         "{:?},{:?},{:?},",
         order_account.id, order_account.maker, order_account.timestart
     );
-    Ok(order_account)
+    Ok((order_pda, order_account))
 }
 pub async fn get_token_vault(
     program: &anchor_client::Program<Rc<Keypair>>,
     token_mint: Pubkey,
-    token_program_id: Pubkey,
+    associated_token_account: Pubkey,
 ) -> Result<Pubkey, anyhow::Error> {
     let (admin_config_pda, _) = Pubkey::find_program_address(&[b"adminconfig"], &bridge::ID);
     let vault_account = associated_token::get_associated_token_address_with_program_id(
         &admin_config_pda,
         &token_mint,
-        &token_program_id,
+        &associated_token_account,
     );
     Ok(vault_account)
+}
+pub async fn get_user_ata(token_mint: Pubkey, user: Pubkey) -> Result<Pubkey, anyhow::Error> {
+    let user_ata = get_associated_token_address(&user, &token_mint);
+    Ok(user_ata)
 }
