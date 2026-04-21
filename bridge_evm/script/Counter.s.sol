5e39e022f0bf169f272c2263574f114017b07992
Nok1luck1 <kolambo4ka@gmail.com>
2026-04-21 19:22:42 +0000

    укиуки

--- bridge_evm/script/Counter.s.sol
+++ bridge_evm/script/Counter.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Bridge} from "../src/Bridge.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    Bridge public counter;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        counter = new Bridge(address(0));
 
         vm.stopBroadcast();
     }
